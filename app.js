b2b60b5034314b5fc699e86e82983aa2e20717c2
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-04 12:38:53 +0200

    buttons logic fixed

--- app.js
+++ app.js
@@ -1,119 +1,114 @@
-
-let bookInput = document.getElementById('book-title-input');
-let generateBtn = document.getElementById('generate-btn');
-let summaryElement = document.getElementById('summary');
-let bookTitleElement = document.getElementById('book-title');
-let summaryModal = document.getElementById('summary-modal');
-let closeBtn = summaryModal.getElementsByClassName('close')[0];
-let saveBtn = document.getElementById('save-btn');
-const closeModalBtn = document.getElementById('close-modal-btn');
-let deleteBtn = document.getElementById('delete-btn');
+let bookInput = document.getElementById("book-title-input");
+let generateBtn = document.getElementById("generate-btn");
+let summaryElement = document.getElementById("summary");
+let bookTitleElement = document.getElementById("book-title");
+let summaryModal = document.getElementById("summary-modal");
+let closeBtn = summaryModal.getElementsByClassName("close")[0];
+let saveBtn = document.getElementById("save-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+let deleteBtn = document.getElementById("delete-btn");
 const spinner = document.getElementById("spinner");
-const bookList = document.getElementById('book-list');
-const myBooksLabel = document.getElementById('my-books-label');
-const languagePicker = document.getElementById('language-picker');
+const bookList = document.getElementById("book-list");
+const myBooksLabel = document.getElementById("my-books-label");
+const languagePicker = document.getElementById("language-picker");
 
+generateBtn.disabled = true;
 // Retrieve saved books from localStorage
-const savedBooks = JSON.parse(localStorage.getItem('books')) || [];
-
-let bookTitle;
-// Update bookTitle when input value changes
-bookInput.addEventListener('change', () => {
-    bookTitle = bookInput.value;
-});    
-
-// Update bookTitle when a book summary is clicked on
-bookTitleElement.addEventListener('DOMSubtreeModified', () => {
-  bookTitle = bookTitleElement.innerHTML;
-});
-
-
-async function fetchSummary(bookTitle, language) {
-  const response = await fetch('https://summary-gpt.onrender.com/api/summarize', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },    
-    body: JSON.stringify({ bookTitle, language }) // Add language to the request body
-  });    
-  const data = await response.json();
-  if (data) {
-    return data;
-  } else {  
-    console.error("Unexpected data format:", data);
-    return "An error occurred while fetching the summary. Please try again.";
-  }    
-}  
-
-
-generateBtn.addEventListener('click', async (e) => {
-e.preventDefault();
-spinner.style.display = "block"; // show the spinner
-bookTitle = bookInput.value; // Get current input value
-const language = languagePicker.value; // Get the selected language
-const summary = await fetchSummary(bookTitle, language); // Pass the language to fetchSummary
-spinner.style.display = "none";
-summaryModal.style.display = 'block';
-summaryElement.innerHTML = summary;
-bookTitleElement.innerHTML = bookTitle;
-  // Show the Close button only if the book title is empty
-  if (!bookTitle) {
-    saveBtn.style.display = 'none';
-    closeModalBtn.style.display = 'block';
-    deleteBtn.style.display = 'none';
-  } else {
-    saveBtn.style.display = 'block';
-    closeModalBtn.style.display = 'none';
-    deleteBtn.style.display = 'block';
-  }
-});
-
+const savedBooks = JSON.parse(localStorage.getItem("books")) || [];
 
 //hide "My Books" label if no books yet saved
 function updateMyBooksLabel() {
   if (savedBooks.length === 0) {
-    myBooksLabel.style.display = 'none';
+    myBooksLabel.style.display = "none";
   } else {
-    myBooksLabel.style.display = 'block';
-  }  
-}  
+    myBooksLabel.style.display = "block";
+  }
+}
 updateMyBooksLabel();
 
+let bookTitle;
+// Update bookTitle when input value changes
+bookInput.addEventListener("change", () => {
+  bookTitle = bookInput.value;
+});
 
+bookInput.addEventListener("input", () => {
+  generateBtn.disabled = !bookInput.value;
+});
 
+// Update bookTitle when a book summary is clicked on
+bookTitleElement.addEventListener("DOMSubtreeModified", () => {
+  bookTitle = bookTitleElement.innerHTML;
+});
 
+async function fetchSummary(bookTitle, language) {
+  const response = await fetch(
+    "https://summary-gpt.onrender.com/api/summarize",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ bookTitle, language }), // Add language to the request body
+    }
+  );
+  const data = await response.json();
+  if (data) {
+    return data;
+  } else {
+    console.error("Unexpected data format:", data);
+    return "An error occurred while fetching the summary. Please try again.";
+  }
+}
+
+generateBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  spinner.style.display = "block"; // show the spinner
+  bookTitle = bookInput.value; // Get current input value
+  const language = languagePicker.value; // Get the selected language
+  const summary = await fetchSummary(bookTitle, language); // Pass the language to fetchSummary
+  spinner.style.display = "none";
+  summaryModal.style.display = "block";
+  summaryElement.innerHTML = summary;
+  bookTitleElement.innerHTML = bookTitle;
+  // Show the Close button only if the book title is empty
+  if (!bookTitle) {
+    saveBtn.style.display = "none";
+    closeModalBtn.style.display = "block";
+    deleteBtn.style.display = "none";
+  } else {
+    saveBtn.style.display = "block";
+    closeModalBtn.style.display = "none";
+    deleteBtn.style.display = "block";
+  }
+});
 
 // Add a new book to localStorage
 function saveBook(title, summary, color) {
   const book = { title, summary, color };
   savedBooks.push(book);
-  localStorage.setItem('books', JSON.stringify(savedBooks));
-}  
-
+  localStorage.setItem("books", JSON.stringify(savedBooks));
+}
 
 // Generate a list of buttons for each saved book title
 savedBooks.forEach((book) => {
-  const button = document.createElement('button');
+  const button = document.createElement("button");
   button.textContent = book.title;
-  button.classList.add('card')
-  button.addEventListener('click', () => {
+  button.classList.add("card");
+  button.addEventListener("click", () => {
     // Display the saved summary in a pop-up
-    summaryModal.style.display = 'block';
+    summaryModal.style.display = "block";
     summaryElement.innerHTML = book.summary;
     bookTitleElement.innerHTML = book.title;
     //hide savebtn and show close button
-    saveBtn.style.display = 'none';
-    closeModalBtn.style.display = 'block';
-  });  
+    saveBtn.style.display = "none";
+    closeModalBtn.style.display = "block";
+  });
 
   // Use the saved color for the button
   button.style.backgroundColor = book.color;
   bookList.appendChild(button);
-});  
-
-
-
-
+});
 
 // Generate random dark color
 function generateRandomColor() {
@@ -124,72 +119,65 @@ function generateRandomColor() {
 }
 
 // Handle save button click
-saveBtn.addEventListener('click', () => {
+saveBtn.addEventListener("click", () => {
   const summary = summaryElement.innerHTML;
   if (bookTitle && summary) {
     const color = generateRandomColor();
     saveBook(bookTitle, summary, color);
+    summaryModal.classList.add("modal-out");
+    // Remove the summary and book title after the animation is finished
+    setTimeout(() => {
+      summaryModal.style.display = "none";
+      summaryModal.classList.remove("modal-out");
+      summaryElement.innerHTML = "";
+      bookTitleElement.innerHTML = "";
+    }, 500);
     // Refresh the book list
     location.reload();
     updateMyBooksLabel();
   }
 });
 
-deleteBtn.addEventListener('click', () => {
-  const confirmDelete = confirm("Are you sure you want to delete this book summary?");
-  
+deleteBtn.addEventListener("click", () => {
+  const confirmDelete = confirm(
+    "Are you sure you want to delete this book summary?"
+  );
+
   if (confirmDelete) {
-      const bookIndex = savedBooks.findIndex((book) => book.summary === bookTitleElement.innerHTML);
-      if (bookIndex !== -1) {
-        savedBooks.splice(bookIndex, 1);
-        localStorage.setItem('books', JSON.stringify(savedBooks));
-        const buttonToRemove = bookList.children[bookIndex];
-        bookList.removeChild(buttonToRemove);
-        summaryModal.style.display = 'none';
-      }
-      updateMyBooksLabel();
+    const bookIndex = savedBooks.findIndex(
+      (book) => book.title === bookTitleElement.innerHTML
+    );
+    if (bookIndex !== -1) {
+      savedBooks.splice(bookIndex, 1);
+      localStorage.setItem("books", JSON.stringify(savedBooks));
+      const buttonToRemove = bookList.children[bookIndex];
+      bookList.removeChild(buttonToRemove);
+      summaryModal.classList.add("modal-out");
+      setTimeout(() => {
+        summaryModal.style.display = "none";
+        summaryModal.classList.remove("modal-out");
+        summaryElement.innerHTML = "";
+      }, 500);
+    }
+    updateMyBooksLabel();
   }
 });
 
-
-
-
-
-
-deleteBtn.addEventListener('click', () => {
-  const confirmDelete = confirm("Are you sure you want to delete this book summary?");
-  
-  if (confirmDelete) {
-      const bookIndex = savedBooks.findIndex((book) => book.title === bookTitleElement.innerHTML);
-      if (bookIndex !== -1) {
-        savedBooks.splice(bookIndex, 1);
-        localStorage.setItem('books', JSON.stringify(savedBooks));
-        const buttonToRemove = bookList.children[bookIndex];
-        bookList.removeChild(buttonToRemove);
-        summaryModal.style.display = 'none';
-      }
-      updateMyBooksLabel();
-  }
-});
-
-
-
-
-closeBtn.addEventListener('click', () => {
-  summaryModal.classList.add('modal-out');
+closeBtn.addEventListener("click", () => {
+  summaryModal.classList.add("modal-out");
   setTimeout(() => {
-    summaryModal.style.display = 'none';
-    summaryModal.classList.remove('modal-out');
-    summaryElement.innerHTML = '';
+    summaryModal.style.display = "none";
+    summaryModal.classList.remove("modal-out");
+    summaryElement.innerHTML = "";
   }, 500);
 });
 
 // Handle Close modal button click
-closeModalBtn.addEventListener('click', () => {
-  summaryModal.classList.add('modal-out');
+closeModalBtn.addEventListener("click", () => {
+  summaryModal.classList.add("modal-out");
   setTimeout(() => {
-    summaryModal.style.display = 'none';
-    summaryModal.classList.remove('modal-out');
-    summaryElement.innerHTML = '';
+    summaryModal.style.display = "none";
+    summaryModal.classList.remove("modal-out");
+    summaryElement.innerHTML = "";
   }, 500);
 });
